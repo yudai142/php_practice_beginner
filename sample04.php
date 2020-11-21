5e778c1cd62522619b98f94324f34bd056c51ac2
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2020-11-21 10:06:01 +0900

    Initial commit

---
+++ sample04.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+date_default_timezone_set("Asia/Tokyo");
+$today = new DateTime();
+print($today->format("G時i分s秒"));
+?>
+</pre>
+</main>
+</body>    
+</html>
