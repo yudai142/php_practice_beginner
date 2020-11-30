6664556edc64439a76e7c0e68817b8838c768908
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2020-11-30 20:43:15 +0900

    Create sample11.php

---
+++ sample11.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+$fruits = [
+  "apple" => "りんご",
+  "grape" => "ぶどう",
+  "lemon" => "レモン"
+];
+
+foreach ($fruits as $val => $name) {
+  print ($val . ":" . $name . "\n");
+}
+
+?>
+</pre>
+</main>
+</body>    
+</html>
